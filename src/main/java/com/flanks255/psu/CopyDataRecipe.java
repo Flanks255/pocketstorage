2efa65d2b5cbbee3eea3148ff5dbda46d3093d13
Flanks255 <32142731+Flanks255@users.noreply.github.com>
2021-05-15 15:09:18 -0500

    Removing AT's

--- src/main/java/com/flanks255/psu/CopyDataRecipe.java
+++ src/main/java/com/flanks255/psu/CopyDataRecipe.java
@@ -1,7 +1,6 @@
 package com.flanks255.psu;
 
 import com.flanks255.psu.items.PocketStorageUnit;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.item.ItemStack;
@@ -9,21 +8,20 @@ import net.minecraft.item.crafting.IRecipeSerializer;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.item.crafting.ShapedRecipe;
 import net.minecraft.network.PacketBuffer;
-import net.minecraft.util.JSONUtils;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.registries.ForgeRegistryEntry;
 
 import javax.annotation.Nullable;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Map;
 
 
 public class CopyDataRecipe extends ShapedRecipe {
     public CopyDataRecipe(final ResourceLocation id, final String group, final int recipeWidth, final int recipeHeight, final NonNullList<Ingredient> ingredients, final ItemStack recipeOutput) {
         super(id, group, recipeWidth, recipeHeight, ingredients, recipeOutput);
+    }
+
+    public CopyDataRecipe(ShapedRecipe shapedRecipe) {
+        super(shapedRecipe.getId(), shapedRecipe.getGroup(), shapedRecipe.getRecipeWidth(), shapedRecipe.getRecipeHeight(), shapedRecipe.getIngredients(), shapedRecipe.getRecipeOutput());
     }
 
     @Override
@@ -53,43 +51,27 @@ public class CopyDataRecipe extends ShapedRecipe {
         @Nullable
         @Override
         public CopyDataRecipe read(ResourceLocation recipeId, PacketBuffer buffer) {
-            int width = buffer.readInt();
-            int height = buffer.readInt();
-            String group = buffer.readString();
-
-            NonNullList<Ingredient> ingredients = NonNullList.withSize(height * width, Ingredient.EMPTY);
-            for (int i = 0; i < ingredients.size(); i++) {
-                ingredients.set(i, Ingredient.read(buffer));
-
-            }
-
-            ItemStack craftingResult = buffer.readItemStack();
-            return new CopyDataRecipe(recipeId,group,width,height,ingredients,craftingResult);
+            return new CopyDataRecipe(IRecipeSerializer.CRAFTING_SHAPED.read(recipeId, buffer));
         }
 
         @Override
         public CopyDataRecipe read(ResourceLocation recipeId, JsonObject json) {
-            String group = JSONUtils.getString(json, "group", "");
-            Map<String, Ingredient> map = ShapedRecipe.deserializeKey(JSONUtils.getJsonObject(json, "key"));
-            String[] pattern = ShapedRecipe.shrink(ShapedRecipe.patternFromJson(JSONUtils.getJsonArray(json, "pattern")));
-            int width = pattern[0].length();
-            int height = pattern.length;
-            NonNullList<Ingredient> ingredients = ShapedRecipe.deserializeIngredients(pattern, map, width, height);
-            ItemStack result = ShapedRecipe.deserializeItem(JSONUtils.getJsonObject(json, "result"));
-
-            return new CopyDataRecipe(recipeId, group,width, height,ingredients,result);
+            try {
+                return new CopyDataRecipe(IRecipeSerializer.CRAFTING_SHAPED.read(recipeId, json));
+            } catch (Exception exception) {
+                PocketStorage.LOGGER.info("Error reading CopyData Recipe from packet: ", exception);
+                throw exception;
+            }
         }
 
         @Override
         public void write(PacketBuffer buffer, CopyDataRecipe recipe) {
-            buffer.writeVarInt(recipe.getRecipeWidth());
-            buffer.writeVarInt(recipe.getRecipeHeight());
-            buffer.writeString(recipe.getGroup());
-            for (Ingredient ingredient: recipe.getIngredients()) {
-                ingredient.write(buffer);
+            try {
+                IRecipeSerializer.CRAFTING_SHAPED.write(buffer, recipe);
+            } catch (Exception exception) {
+                PocketStorage.LOGGER.info("Error writing CopyData Recipe to packet: ", exception);
+                throw exception;
             }
-            buffer.writeItemStack(recipe.getRecipeOutput());
         }
     }
-
 }
